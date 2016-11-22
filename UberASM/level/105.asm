a5c8ad464fb669d56cb737eeed7fb7dc76e5d94c
Dolphish Dolphinium <dolphinspray.fr@gmail.com>
2016-11-21 21:15:32 -0500

    Updated the level asm.
    Fixed a bug.

--- UberASM/level/105.asm
+++ UberASM/level/105.asm
@@ -1,15 +1,18 @@
 ; Runs on level load
  
- INIT:
-	JMP Reset_Lives
-	RTL
+INIT:
+    JSL Coin_Gate_Count
+    JSL Clear_Coins
+    JSL Reset_Lives
+    JSL Reset_Powerup
+    RTL
 
 ; Sets coins required to pass through a gate
  
- Coin_Gate_Count:
-	LDA #$0F ; number of coins required to pass through a coin gate, in hex
-	STA $0DD4
-	RTL
+Coin_Gate_Count:
+    LDA #$00 ; number of coins required to pass through a coin gate, in hex
+    STA $0DD4
+    RTL
 
 ; Clears Mario's coin count when he enters the level
 
@@ -20,7 +23,15 @@ Clear_Coins:
 
 ; Sets the player's life count
  
- Reset_Lives:
-	LDA #$62 ; the number of lives, in hex
-	STA $0DBE
-	RTL
+Reset_Lives:
+    LDA #$62 ; the number of lives, in hex
+    STA $0DBE
+    RTL
+
+; Removes the player's powerup status
+
+Reset_Powerup:
+    LDA #$00
+    STA $0019  ; player powerup state
+    STA $0DC2 ; player stored powerup
+    RTL
