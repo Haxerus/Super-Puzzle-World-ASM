b332d286d6253d76691b72a250d9887429487d32
Dolphish Dolphinium <dolphinspray.fr@gmail.com>
2016-12-11 01:58:38 -0500

    Add files via upload

---
+++ UberASM/level/10B.asm
@@ -0,0 +1,39 @@
+; Runs on level load
+ 
+INIT:
+    JSL Coin_Gate_Count
+    JSL Clear_Coins
+    JSL Reset_Lives
+    JSL Reset_Powerup
+    RTL
+
+; Sets coins required to pass through a gate
+ 
+Coin_Gate_Count:
+    LDA #$1D ; number of coins required to pass through a coin gate, in hex
+    STA $0DD4
+    RTL
+
+; Clears Mario's coin count when he enters the level
+
+Clear_Coins:
+    LDA #$00
+    STA $0DBF
+    RTL
+
+; Sets the player's life count
+ 
+Reset_Lives:
+    LDA #$62 ; the number of lives, in hex
+    STA $0DBE
+    RTL
+
+; Removes the player's powerup status
+
+Reset_Powerup:
+    LDA #$00
+    STA $0019  ; player powerup state
+
+    LDA #$00   ; don't store a powerup
+    STA $0DC2 ; player stored powerup
+    RTL
